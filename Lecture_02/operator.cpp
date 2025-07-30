cd591566f78e39ae1648a3c39bf56adc6ea1751a
Rishav <rishavofficial0709@gmail.com>
2025-07-30 06:45:01 +0000

    .

--- Lecture_02/operator.cpp
+++ Lecture_02/operator.cpp
@@ -2,12 +2,22 @@
 using namespace std;
 
 int main() {
-    int a = 5;
-    int b = 10;
 
-    cout << a+b<< endl;
-    cout << a-b<< endl;
-    cout << b/a<< endl;
-    cout << a%b<< endl;
-    cout << a*b<< endl;
+    int marks;
+    cout << "Enter marks here: "<< endl;
+    cin >> marks;
+    cout << "printing marks: "<< marks << endl;
+    // int a = 5;
+    // int b = 10;
+
+    // cout << a+b<< endl;
+    // cout << a-b<< endl;
+    // cout << b/a<< endl;
+    // cout << a%b<< endl;
+    // cout << a*b<< endl;
+
+    // arithmetic operation  = (+,-,/,*,%)
+    // Relational operation = (>,<,<=,>=,!=,==)
+    // Assignment operation = (=)
+    // logic operation = (&&,||,!)
 }
