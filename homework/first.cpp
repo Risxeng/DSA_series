8ac6fa5fc619d3301d32c51a580a783a1672bd60
Rishav <rishavofficial0709@gmail.com>
2025-08-02 09:45:07 +0000

    .

---
+++ homework/first.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int row = 0; row < n ; row++) { 
+
+        for (int space = 0; space < n - row - 1; space++) {
+            cout << "_";
+        }
+
+        for (int col = 0; col < 2 * row + 1; col++) {
+            if (col == 0 || col == 2 * row) {
+                cout << "*";
+            } else {
+                cout << "_";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
