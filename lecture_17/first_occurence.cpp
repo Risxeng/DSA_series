4cffdf6bd65a53fd0142127336212418a73d0e24
Rishav <rishavofficial0709@gmail.com>
2025-08-19 08:47:43 +0000

    .

--- lecture_17/first_occurence.cpp
+++ lecture_17/first_occurence.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 using namespace std;
-int find_first_occurence(int arr[] , int n, int target) {
 
-    int s = 0;
-    int e = n-1;
+int binarysearch(int arr[] , int n , int target) {
+    int start = 0;
+    int end = n-1;
 
-    int mid = (s+e)/2;
+    int mid;
 
-    int ans = -1;
-
-    while (s<=e)
+    while (start <= end)
     {
-        if(arr[mid] == target){
-            ans == mid;
-            e = mid-1;
+        mid = (start + end) / 2;
+        if (arr[mid] == target)
+        {
+            return mid;
         }
-        else if(target > arr[mid]){
-            s = mid+1;
+        else if (target > arr[mid])
+        {
+            start = mid + 1;
         }
-        else if(target < arr[mid]) {
-            e =mid-1;
+        else
+        {
+            end = mid - 1;
         }
-        mid = (s+e)/2;
+        mid = (start + end) / 2;
     }
-    return ans;
+    return -1;
     
 }
+
 int main () {
-    int arr[] = {10,20,30,30,30,30,70,80,90,100};
-    int target = 70;
+    int arr[] = {10,20,30,40,50,60,70,80,90,100};
+    int target = 30;
     int n =10;
 
-    int ans = find_first_occurence (arr,n,target);
+    int ans = binarysearch (arr,n,target);
 
     if (ans == -1) 
     {
