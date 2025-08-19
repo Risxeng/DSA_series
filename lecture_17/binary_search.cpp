d2dfeaca6ac0fbeba221b621ea5d03e0ee335f70
Rishav <rishavofficial0709@gmail.com>
2025-08-19 08:11:23 +0000

    .

---
+++ lecture_17/binary_search.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int binarysearch(int arr[] , int n , int target) {
+    int start = 0;
+    int end = n-1;
+
+    int mid = (start+end)/2;
+
+    while (start <= end) {}
+    {
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        else if (target > arr[mid])
+        {
+            start = mid+1;
+        }
+        else if (target < arr[mid]){
+            end = mid - 1;
+        }
+        mid = (start+end)/2 ;
+        
+    }
+    return -1;
+    
+}
+
+int main () {
+    int arr[] = {10,20,30,40,50,60,70,80,90,100};
+    int target = 100;
+    int n =10;
+
+    int ans = binarysearch (arr,n,target);
+
+    if (ans == -1) 
+    {
+        cout << "Element not found " << endl;
+    }else {
+        cout << "Element found at Index: " << ans << endl;
+    }
+    return 0;
+}
