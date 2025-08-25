573fec8ee2d17e1d5c021bee035c9a45d79870a6
Rishav <rishavofficial0709@gmail.com>
2025-08-25 09:19:04 +0000

    .

--- lecture_23/string_two.cpp
+++ lecture_23/string_two.cpp
@@ -4,9 +4,61 @@ using namespace std;
 int main() {
     // string name;
     // cin >> name;
-    string temp = "";
-    cout << "length of the string is: " << temp.length() << endl;
-    cout << "string is empty or not: " << temp.empty() << endl;
+    // // string temp = "";
+    // cout << "length of the string is: " << name.length() << endl;
+    // cout << "string is empty or not: " << name.empty() << endl;
+    // cout << "character at the position 0 is: " << name.at(0) << endl;
+    // cout << "front character of string is: " << name.front() << endl;
+    // cout << "back character of string is: " << name.back() << endl;
 
+
+    // string str1 = "rishav";
+    // string str2 = "RISHAV";
+    // str1.append(str2);
+
+    // cout << "After appending" << endl;
+    // cout << str1 << endl;
+    // cout << str2 << endl;
+
+    // string name = "rishav";
+    // string middle = "kumar";
+
+    // name.insert(5,middle);
+    // cout << "printing name: " << name << endl;
+
+    // string name = "love";
+    // cout << name << endl;
+
+    // name.push_back('R');
+    // cout << name << endl;
+
+    // name.pop_back();
+    // cout << name << endl;
+
+    // string str1 = "yaar tera superstar desi kalakar";
+    // string str2 = "desi";
+    // if(str1.find(str2)==string::npos)
+    // {
+    //     cout << "not found" << endl;    
+    // }
+    // else{
+    //     cout << "found" << endl;
+    // }
+
+    // string str1 = "rishav";
+    // string str2 = "rishav";
+
+    // if (str1.compare(str2) == 0)
+    // {
+    //     cout << "matching" << endl;
+    // }else {
+    //     cout << "not matching" << endl;
+    // }
+
+    // string desc = "this is a car, big daddy of all suvs";
+    // cout << desc.substr(19,5) << endl;
+    
+    
+    
     
 }
