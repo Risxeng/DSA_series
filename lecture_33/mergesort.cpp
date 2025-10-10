79a6eb275d5e0baf9484922c32dee4cf0b52c11b
Rishav <rishavofficial0709@gmail.com>
2025-10-10 11:52:56 +0000

    .

---
+++ lecture_33/mergesort.cpp
@@ -0,0 +1,36 @@
+                            //merge sort
+                            
+#include <iostream>
+using namespace std;
+void merge(int arr[], int i, int j){
+    int  mid = (i+j)/2;
+
+    int lenLeft = mid-
+
+}
+void mergesort(int arr[], int i, int j){
+    if (i > j){
+        return;
+    }
+    if(i == j){
+        return;
+    } 
+    int mid = (i+j)/2;
+
+    mergesort(arr, i , mid);
+    mergesort(arr, mid+1 , j);
+
+    merge(arr, i, j);
+}
+
+
+int main() {
+
+    int arr[] = {2,1,6,9,4,5};
+    int size = 6;
+    int i = 0;
+    int j = size - 1;
+
+    mergesort(arr, i, j);
+
+}
