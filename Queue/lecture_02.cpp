f41e034e46afb8391d4a61a827e106e28519c24c
Rishav <rishavofficial0709@gmail.com>
2025-12-20 18:27:02 +0000

    .

--- Queue/lecture_02.cpp
+++ Queue/lecture_02.cpp
@@ -28,27 +28,27 @@ using namespace std;
 //     q.push(top);
 // }
 
-void reverseKElement(queue<int> &q, int k){
-    int n = 8;
-    if(k > n || k ==0){
-        return ;
-    }
-    stack<int> s;
-    while(k--){
-        s.push(q.front());
-        q.pop();
-    }
-    while(!s.empty()){
-        q.push(s.top());
-        s.pop();
-    }
-    for (int i = 0; i < (n-k); i++)
-    {
-        int front = q.front();
-        q.pop();
-        q.push(front);
-    }
-}
+// void reverseKElement(queue<int> &q, int k){
+//     int n = 8;
+//     if(k > n || k ==0){
+//         return ;
+//     }
+//     stack<int> s;
+//     while(k--){
+//         s.push(q.front());
+//         q.pop();
+//     }
+//     while(!s.empty()){
+//         q.push(s.top());
+//         s.pop();
+//     }
+//     for (int i = 0; i < (n-k); i++)
+//     {
+//         int front = q.front();
+//         q.pop();
+//         q.push(front);
+//     }
+// }
 
 int main(){
     queue<int> q;
@@ -80,12 +80,14 @@ int main(){
     //     cout << q.front() << " ";
     //     q.pop();
     // }
-    reverseKElement(q, 3);
     
-    while (!q.empty())
-    {
-        cout << q.front() << " ";
-        q.pop();
-    }
+    // reverseKElement(q, 3);
+    
+    // while (!q.empty())
+    // {
+    //     cout << q.front() << " ";
+    //     q.pop();
+    // }
+    
     return 0;
 }
--- Queue/two_que.cpp
+++ Queue/two_que.cpp
@@ -3,7 +3,54 @@
 #include<stack>
 using namespace std;
 
+// void interLeaveQueue(queue<int> &first) {
+//     queue<int> second;
+//     int n = first.size();
+
+//     for(int i=0; i<n/2; i++){
+//         int temp = first.front();
+//         first.pop();
+//         second.push(temp);
+//     }
+//     while (!second.empty()){
+//         int temp  = second.front();
+//         second.pop();
+//         first.push(temp);
+//         temp = first.front();
+//         first.pop();
+//         first.push(temp);
+//     }
+    
+// }
+
+printFirstNeg(int *arr, int n, int k) {
+    
+}
+
 int main(){
     
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+
+    // interLeaveQueue(q);
+
+    // while (!q.empty())
+    // {
+    //     cout << q.front() << " ";
+    //     q.pop();
+    // }
+
+    int arr[7] = {2,-5,4,-1,-2,0,5};
+
+    int n = 7;
+    int k = 3;
+
+    printFirstNeg(arr,n,k);
+
     return 0;
 }
