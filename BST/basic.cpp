cfc9966a21523c689b665b89b935d12d99a0bd1e
Rishav <rishavofficial0709@gmail.com>
2026-01-14 20:42:58 +0000

    .

--- BST/basic.cpp
+++ BST/basic.cpp
@@ -97,6 +97,102 @@ void postorder(Node* root){
     cout << root->data << " ";
 }
 
+Node* minval(Node* root) {
+
+    if(root == NULL){
+        cout << "No Min Value" << endl;
+        return NULL;
+    }
+    Node* temp = root;
+
+    while(temp->left != NULL){
+        temp = temp->left;
+    }
+    return temp;
+}
+
+Node* maxval(Node* root) {
+    if(root == NULL){
+        cout << "No max value" << endl;
+        return NULL;
+    }
+    Node* temp = root;
+
+    while (temp->right != NULL){
+        temp = temp->right;
+    }
+    return temp;
+}
+
+bool searchInBST(Node* root, int target){
+    //base case
+    if(root == NULL){
+        return false;
+    }
+    //1 case mujhe solve krna hai
+    if(root->data == target){
+        return true;
+    }
+    //baaki recursion sambhal lega
+    //left ya right
+    bool leftAns = false;
+    bool rightAns = false;
+    if(target > root->data){
+        rightAns = searchInBST(root->right, target);
+    }else{
+        leftAns = searchInBST(root->left, target);
+    }
+    return leftAns || rightAns;
+
+}
+Node* delteFromBST(Node* root, int target){
+    //target ko dhundo
+    //target ko delete kro
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->data == target)
+    {
+        //ab delete krege
+        //4 cases
+
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+        else if (root->left != NULL && root->right == NULL)
+        {
+            Node* childSubtree = root->left;
+            delete root;
+            return childSubtree;
+        }
+        else if (root->left == NULL && root->right != NULL)
+        {
+            Node* childSubtree = root->right;
+            delete root;
+            return childSubtree;
+        }
+        else{
+            Node* maxi = maxValue(root->left);
+            root->data = maxi->data;
+            root->left = delteFromBST(root->left, maxi->data);
+            return root;
+        }
+        
+
+    }
+    else if(target > root->data){
+        root->left = delteFromBST(root->left, target);
+    }
+    else{
+        root->right = delteFromBST(root->right, target);
+    }
+    
+    
+}
+
 int main(){
 
     Node* root = NULL;
@@ -110,6 +206,52 @@ int main(){
     preorder(root);
     cout << endl << "postorder: " ;
     postorder(root);
+
+    cout << endl;
+    Node* minNode = minval(root);
+    if(minNode == NULL){
+        cout << "there is no node in tree, so no min value" << endl;
+    }else{
+        cout << "Min Value: " << minNode->data << endl;
+    }
+
+    cout << endl;
+    Node* maxNode = maxval(root);
+    if(maxNode == NULL){
+        cout << "there is no node in tree, so no max value" << endl;
+    }else{
+        cout << "Max Value: " << maxNode->data << endl;
+    }
+
+    int target ;
+    cout << "enter the value of target: " << endl;
+    cin >> target;
+
+    while(target != -1) {
+        root = delteFromBST(root, target);
+        cout << endl << "Printing level Order Traversal: " << endl;
+        levelOrderTraversal(root);
+
+        cout << "Enter the value of target: " << endl;
+        cin  >> target;
+    }
+
+    // int t;
+    // cout << "enter the target: " << endl;
+    // cin >> t;
+
+    // while (t != -1)
+    // {
+    //     bool ans = searchInBST(root, t);
+    //     if(ans == true) {
+    //         cout << "found" << endl;
+    //     }else{
+    //         cout << "not found" << endl;
+    //     }
+    //     cout << "enter the target: " << endl;
+    //     cin >> t;
+    // }
+    
 
     return 0;
 
