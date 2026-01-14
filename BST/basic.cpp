ad67f0d3ddcda6fe836ff18567d9533c0bab1cb9
Rishav <rishavofficial0709@gmail.com>
2026-01-14 06:25:33 +0000

    .

--- BST/basic.cpp
+++ BST/basic.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 class Node{
@@ -14,6 +15,102 @@ class Node{
     }
 };
 
-Node* createBST(){
-    
+Node* insertIntoBST(Node* &root, int data) {
+
+    if(root == NULL) {
+        root = new Node(data);
+        return root;
+    }
+    if(data > root->data) {
+        root->right = insertIntoBST(root->right, data);
+    }
+    else {
+        root->left = insertIntoBST(root->left, data);
+    }
+    return root;
+}
+void createBST(Node* &root){
+    cout << "Enter data: " << endl;
+    int data;
+    cin >> data;
+
+    while (data != -1) 
+    {
+        insertIntoBST(root , data);
+        cout << "Enter data: " << endl;
+        cin >> data;
+    }
+}
+
+void levelOrderTraversal(Node* root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) {
+            cout << endl;  
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }else{
+            cout << temp->data << " ";
+            if(temp->left != NULL) {
+                q.push(temp->left);
+            }
+            if(temp->right != NULL){
+               q.push(temp->right); 
+            }
+        }
+    }
+}
+void inorder(Node* root){
+    //LNR
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node* root){
+    //NLR
+    if(root == NULL){
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(Node* root){
+    //LRN
+    if(root == NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main(){
+
+    Node* root = NULL;
+    createBST(root);
+
+    levelOrderTraversal(root);
+
+    cout << endl << "Inorder: ";
+    inorder(root);
+    cout << endl << "preorder: ";
+    preorder(root);
+    cout << endl << "postorder: " ;
+    postorder(root);
+
+    return 0;
+
 }
